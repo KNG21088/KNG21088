c2185a4c92d0bd82fc914fb36cc2399f0f5b2a2d
KNG21088 <87695618+KNG21088@users.noreply.github.com>
2021-07-21 10:51:03 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,16 @@
+
+public class addition {
+	public static void main(String[] args) {
+		System.out.println("Enter two numbers");
+	    int first = 200;
+	    int second = 300;
+	    System.out.println(first + " + " + second);
+	    int sum = first + second;
+	    System.out.println("The sum is: " + sum);
+	    
+	    
+	   
+		
+	}
+
+}
